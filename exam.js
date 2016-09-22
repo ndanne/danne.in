5332e1e5f8cfdb37a92f5b429a1f7d7b5d4313ee
Nagaraju Danne <nagaraju.danne@gmail.com>
2016-09-22 18:29:23 +0530

    Update exam.js

--- exam.js
+++ exam.js
@@ -1,7 +1,6 @@
-var pos = 0, test=[], test_status, question, choice, choices, chA, chB, chC = null, chD=null, correct = 0;
-var timeoutID;
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC = null, chD=null, correct = 0, wrong=0;
 var questions = [
-	[ "Are you satsified with exam", "Yes", "No", null, null, "A"],
+	[ "Are you ready for exam?", "Yes", "No", null, null, "A"],
     [ "What is 10 + 4?", "12", "14", "16", null, "B" ],
 	[ "What is 20 - 9?", "7", "13", "11", null, "C" ],
 	[ "What is 7 x 3?", "21", "24", "25", null, "A" ],
@@ -15,57 +14,59 @@ function _(x){
 function renderQuestion(){
 	test = _("test");
 	
-	if(pos >= questions.length){
-		test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
-		_("test_status").innerHTML = "Test Completed";
-		pos = 0;
-		correct = 0;
-		return false;
-	}
+	correct = 0;
 	_("test").style.backgroundColor = "#fff";
-	_("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+	_("test_status").innerHTML = "Total Question "+questions.length;
 	for(var i=0; i < questions.length; i++){
 		//test.innerHTML +="";
-	question = questions[i][0];
-	chA = questions[i][1];
-	chB = questions[i][2];
-	chC = questions[i][3];
-	chD = questions[i][4];
-	
-		
-		test.innerHTML += "<div id='qus"+[i]+"'> <h3>"+question+"</h3>";
-		test.innerHTML += "A. <input type='radio' name='choices"+[i]+"' value='A'> "+chA+"<br>";
-		test.innerHTML += "B. <input type='radio' name='choices"+[i]+"' value='B'> "+chB+"<br>";
+		question = questions[i][0];
+		chA = questions[i][1];
+		chB = questions[i][2];
+		chC = questions[i][3];
+		chD = questions[i][4];	
+		//<div class='ques' id='qus"+i+"'>
+		test.innerHTML += "<h3>"+[i+1]+") "+question+"</h3>";
+		test.innerHTML += "<label id='optA"+i+"'>A. <input type='radio' name='choices"+i+"' onclick='checkAnswer("+i+", `optA"+i+"`)' value='A'> "+chA+"</label>";
+		test.innerHTML += "<label id='optB"+i+"'>B. <input type='radio' name='choices"+i+"' onclick='checkAnswer("+i+", `optB"+i+"`)' value='B'> "+chB+"</label>";
 		if(chC != null){
-			test.innerHTML += "C. <input type='radio' name='choices"+[i]+"' value='C'> "+chC+"<br>";
+			test.innerHTML += "<label id='optC"+i+"'>C. <input type='radio' name='choices"+i+"' onclick='checkAnswer("+i+", `optC"+i+"`)' value='C'> "+chC+"</label>";
 		}
 		if(chD != null){
-			test.innerHTML += "D. <input type='radio' name='choices"+[i]+"' value='D'> "+chD+"<br>";
+			test.innerHTML += "<label id='optD"+i+"'>D. <input type='radio' name='choices"+i+"' onclick='checkAnswer("+i+", `optD"+i+"`)' value='D'> "+chD+"</label>";
 		}
-		test.innerHTML += "<br><button onclick='checkAnswer("+[i]+")'>Submit Answer</button>";
-		test.innerHTML += " </div>";
+		
+		test.innerHTML += "<hr/>";		
 	}
+
+	test.innerHTML += "<br><button name='scores' class='btn' onclick='checkSore()'>Check Score</button>";	
 }
 
-function checkAnswer(num){	
-	
+function checkAnswer(num, optId){
 	var choices = document.getElementsByName("choices"+num);
 	for(var i=0; i<choices.length; i++){
 		if(choices[i].checked){
 			choice = choices[i].value;
 		}
+		choices[i].disabled = true;
 	}
 
 	if(choice == questions[num][5]){
-		_("qus"+num).style.backgroundColor = "#9CBA7F";
+		_(optId).style.backgroundColor = "#9CBA7F";
 		correct++;
 	} else{
-		_("qus"+num).style.backgroundColor = "#FF6666";
-	}
-	pos++;
+		_(optId).style.backgroundColor = "#FF6666";
+		wrong++;
+	}	
 
-	//timeoutID = setTimeout(function(){ renderQuestion() }, 1000);
+	
 }
-
-window.clearTimeout(timeoutID);
+function checkSore(){
+	var checqus = correct+wrong;
+	if(questions.length > checqus){
+		alert("You did not anwer "+ (questions.length - checqus )+" questions")
+	}
+	_("score").innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+	_("score").innerHTML += "<p>Test Completed<p>";
+	window.scrollTo(0,document.body.scrollHeight);
+}
 window.addEventListener("load", renderQuestion, false);
